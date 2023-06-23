3e08eeadb434ee935128b5480e7d149e18d78960
Sylvester <36532468+SylvesterWilmott@users.noreply.github.com>
2023-06-23 22:24:57 +0100

    Fix grid size updated (#12)
    * Update popup.js
    
    * Update popup.js
    
    * Update storage.js

--- src/js/storage.js
+++ src/js/storage.js
@@ -7,38 +7,6 @@ export const preferenceDefaults = {
   win_padding: { status: true, type: 'checkbox' }
 }
 
-export function saveSession (key, value) {
-  return new Promise((resolve, reject) => {
-    chrome.storage.session.set(
-      {
-        [key]: value
-      },
-      function () {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError.message)
-        }
-        resolve()
-      }
-    )
-  })
-}
-
-export function loadSession (key, defaults) {
-  return new Promise((resolve, reject) => {
-    chrome.storage.session.get(
-      {
-        [key]: defaults
-      },
-      function (value) {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError.message)
-        }
-        resolve(value[key])
-      }
-    )
-  })
-}
-
 export function save (key, value) {
   return new Promise((resolve, reject) => {
     chrome.storage.local.set(
@@ -74,17 +42,6 @@ export function load (key, defaults) {
 export function clear (key) {
   return new Promise((resolve, reject) => {
     chrome.storage.local.remove(key, function () {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError.message)
-      }
-      resolve()
-    })
-  })
-}
-
-export function clearSession (key) {
-  return new Promise((resolve, reject) => {
-    chrome.storage.session.remove(key, function () {
       if (chrome.runtime.lastError) {
         reject(chrome.runtime.lastError.message)
       }
