81b84fde8b723854542d35a600594dde146973d4
Sylvester <36532468+SylvesterWilmott@users.noreply.github.com>
2023-06-25 01:36:22 +0100

    Add Edge build target (#13)
    * Add edge build target
    
    * Update version
    
    * Update popup.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,68 @@
 'use strict'
 
 const gulp = require('gulp')
+const path = require('path')
+const pkg = require('./package.json')
+const colors = require('colors/safe')
 const zip = require('gulp-zip')
 const imagemin = require('gulp-imagemin')
-const pkg = require('./package.json')
 const jsonEditor = require('gulp-json-editor')
-const path = require('path')
+const cheerio = require('gulp-cheerio')
+const gulpIf = require('gulp-if')
 const { exec } = require('child_process')
 
-gulp.task('build', function () {
-  // Get the path to the manifest file
-  const manifestPath = path.join(__dirname, 'src', 'manifest.json')
+colors.setTheme({
+  error: 'red'
+})
 
-  // Read the version from the extension manifest
+function logMessage (message) {
+  console.log(`${message}`)
+}
+
+gulp.task('update-version', function (cb) {
+  const manifestPath = path.join(__dirname, 'src', 'manifest.json')
   const manifest = require(manifestPath)
   const version = manifest.version
 
-  // Update the version in package.json
   return gulp.src('./package.json')
     .pipe(jsonEditor({ version }))
     .pipe(gulp.dest('./'))
     .on('end', function () {
-      const filename = `${pkg.name}-v${version}.zip`
-      gulp.src(['src/**', '!src/**/*.map'])
-        .pipe(imagemin([imagemin.optipng({ optimizationLevel: 5 })]))
-        .pipe(zip(filename))
-        .pipe(gulp.dest('build'))
-        .on('end', function () {
-          // Run npm install
-          console.log('Running npm install...')
-          exec('npm install', function (err, stdout, stderr) {
-            if (err) {
-              console.error(`Error running npm install: ${err}`)
-              return
-            }
-            console.log(stdout)
-            console.error(stderr)
-          })
-        })
+      exec('npm install', { cwd: './' }, function (error) {
+        if (error) {
+          logMessage(colors.error('Error running npm install: ' + error))
+          return
+        }
+        cb()
+      })
+    })
+    .on('error', function (err) {
+      logMessage(colors.error('Error updating version in package.json: ' + err.toString()))
     })
 })
+
+gulp.task('build-chrome', function () {
+  const version = require('./package.json').version
+
+  return gulp.src(['src/**'])
+    .pipe(imagemin([imagemin.optipng({ optimizationLevel: 5 })]))
+    .pipe(gulpIf('*.html', cheerio(($) => {
+      $('body').addClass('chrome')
+    })))
+    .pipe(zip(`${pkg.name}-v${version}-chrome.zip`))
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('build-edge', function () {
+  const version = require('./package.json').version
+
+  return gulp.src(['src/**'])
+    .pipe(imagemin([imagemin.optipng({ optimizationLevel: 5 })]))
+    .pipe(gulpIf('*.html', cheerio(($) => {
+      $('body').addClass('edge')
+    })))
+    .pipe(zip(`${pkg.name}-v${version}-edge.zip`))
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('build', gulp.series('update-version', gulp.parallel('build-chrome', 'build-edge')))
