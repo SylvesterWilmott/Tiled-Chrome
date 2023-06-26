cea0fd2de0611e55785412184b301ea0d3e0ca67
Sylvester <36532468+SylvesterWilmott@users.noreply.github.com>
2023-06-26 01:35:59 +0100

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -135,13 +135,13 @@ class Grid {
     this.onCellMouseenterBound = this.onCellMouseenter.bind(this);
   }
   
-  async setup (gridSize) {
+  setup (gridSize) {
     this.buildGrid(gridSize)
     this.attachEventListeners()
     this.attachCellListeners()
   }
 
-  async buildGrid (gridSize) {
+  buildGrid (gridSize) {
     const table = document.getElementById('grid')
 
     table.innerHTML = ''
@@ -701,4 +701,45 @@ async function onSelectChanged (e) {
     gridInstance.attachEventListeners()
     gridInstance.attachCellListeners()
   }
+}
+
+async function getDisplayContainingCurrentWindow (connectedDisplays, currentWindow) {
+  let index = 0
+  let maxCornersContained = 0
+
+  // Get the coordinates of all four corners of the current window
+  const wt = currentWindow.top
+  const wl = currentWindow.left
+  const wr = currentWindow.width + wl
+  const wb = currentWindow.height + wt
+  const corners = [
+    [wt, wl],
+    [wt, wr],
+    [wb, wl],
+    [wb, wr]
+  ]
+
+  // Iterate over the connectedDisplays array and find the display that contains the most corners of the current window
+  for (const [i, display] of connectedDisplays.entries()) {
+    const dt = display.bounds.top
+    const dl = display.bounds.left
+    const dr = display.bounds.width + dl
+    const db = display.bounds.height + dt
+
+    // Check how many corners of the current window are contained within the current display
+    let cornersContained = 0
+    for (const [y, x] of corners) {
+      if (y >= dt && y <= db && x >= dl && x <= dr) {
+        cornersContained++
+      }
+    }
+
+    // Update the selected display if the current display contains more corners than the previous selection
+    if (cornersContained > maxCornersContained) {
+      index = i
+      maxCornersContained = cornersContained
+    }
+  }
+
+  return connectedDisplays[index]
 }
